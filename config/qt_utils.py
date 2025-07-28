0b3124b0bb2924bd1ff752e33cde08c1a16e55d2
KyroElkiya <andrew-whatling@hotmail.co.uk>
2025-07-28 17:38:43 +0100

    storing config into an old directory to redesign how system will work

--- config/qt_utils.py
+++
@@ -1,6 +0,0 @@
-from PySide6.QtCore import *
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-from PySide6.QtUiTools import *
-import os, sys, json, shutil, subprocess, time
