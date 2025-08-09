d28aeef52694387cc18ebca7813cadf93d4358e5
KyroElkiya <andrew-whatling@hotmail.co.uk>
2025-08-09 13:30:40 +0100

    Added OCIO json and startup.py

--- main/startup/ocio_startup.py
+++ main/startup/ocio_startup.py
@@ -3,6 +3,15 @@ from utils import startup_utils
 
 
 def main(root: str):
+    
+    # Loading data from ocio json file in config folder
 
     data = startup_utils.load_config_users_json(root, "ocio")
+    aces_script = data["aces_ocio_env_script"]
+
+    # Running ocio batch script through subprocess
+    # Double check this may not fully work when setting OCIO env variable <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    subprocess.run(aces_script)
+
 
